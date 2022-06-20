fa900d5d66e65e7cee6dd900decb9740e53db31f
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-20 22:27:00 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const app = express();
+
+// middleware
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+// SET UP MONGOOSE CONNECTION------------------
+const url = `mongodb://localhost:27017/wikiDB`;
+// connect to mongoose
+mongoose.connect(url, function (err) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Connected to MongoDB`);
+});
+// --------------------------------------------
+
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`app is listening at port ${port}`)
+})
