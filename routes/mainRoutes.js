c9508f2572d519653f063e58d680b98515d8431e
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2022-06-21 04:18:10 +0100

    update

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,33 +2,46 @@ const Article = require("../model/schema");
 const { Router } = require("express");
 const router = Router();
 
-// get all articles
-router.get("/articles", (req, res) => {
+router
+  .route("/articles")
+
+  .get((req, res) => {
+    // get all articles
 
     Article.find({}, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            // res.render("index", { articles: data });
-            res.send(data);
-        }
-    })
-})
-
-// post new article
-router.post("/articles", (req, res) => {
+      if (err) {
+        console.log(err);
+      } else {
+        // res.render("index", { articles: data });
+        res.send(data);
+      }
+    });
+  })
+  .post((req, res) => {
+    // post new article
     const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    })
-    newArticle.save((err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.redirect("/articles");
-        }
-    })
-})
-
+      title: req.body.title,
+      content: req.body.content,
+    });
+    newArticle.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(`successfully added new article`);
+        res.redirect("/articles");
+      }
+    });
+  })
+  .delete((req, res) => {
+    // delete all article
+    Article.deleteMany({}, (err) => {
+      // to see if it works check the console
+      if (!err) {
+        res.send(`successfully deleted all articles`);
+      } else {
+        console.log(err);
+      }
+    });
+  });
 
 module.exports = router;
